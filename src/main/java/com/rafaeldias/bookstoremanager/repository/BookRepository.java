7f41ae6d416bc0285f4269ee43b65e3935ff7ce1
Rafael Dias <rafaeldias0934@gmail.com>
2022-07-23 20:18:46 -0300

    Created book repository class

---
+++ src/main/java/com/rafaeldias/bookstoremanager/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.rafaeldias.bookstoremanager.repository;
+
+import com.rafaeldias.bookstoremanager.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
